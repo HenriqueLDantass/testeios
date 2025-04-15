44114c2e92c8feeabaf401eee34d1c15925499c5
Henrique Dantas <henrique.lemos@zoomeducacaocorporativa.com.br>
2025-04-15 13:19:12 -0300

    libs

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,8 +3,15 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  battery_plus
   cloud_firestore
   firebase_core
+  flutter_inappwebview_windows
+  flutter_secure_storage_windows
+  local_auth_windows
+  permission_handler_windows
+  share_plus
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
